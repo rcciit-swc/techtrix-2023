3276c06fc9b75f64ba2402cca458f81e1a3520e7
Arghya <arghyadutta080@gmail.com>
2023-03-21 02:36:08 +0530

    fix images

--- components/LandingPage/Events.tsx
+++ components/LandingPage/Events.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import style from '@/styles/Event.module.css'
+import Image from 'next/image'
+// import img1 from '@/images/landingpage/Tetrix automata.svg'
 
 const Events = () => {
     return (
         <div>
             <div className={style.wrapper}>
                 <div className={`${style.box} ${style.a}`} >
-                    <div id="a">A</div>
+                    <div id="a"><Image src="../images/landingpage/Techtrix robotics.svg" height={100} width={100} alt="" priority /></div>
                 </div>
                 <div className={`${style.box} ${style.b}`} >
                     <div id="b">B</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import Events from "@/components/landingPage/Events";
+import Events from "@/components/LandingPage/Events";
 import data from "../public/data.json";
 
 
