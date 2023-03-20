20a11bea0a47c8c24765f478d1abddf3ba4f2282
Arghya <arghyadutta080@gmail.com>
2023-03-21 03:37:12 +0530

    modification required in styling in event section

--- components/LandingPage/Events.tsx
+++ components/LandingPage/Events.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import style from '@/styles/Event.module.css'
 import Image from 'next/image'
-// import img1 from '@/images/landingpage/Tetrix automata.svg'
+import img1 from '@/images/landingpage/automata.svg'
+import img2 from '@/images/landingpage/outofthebox.svg'
+import img3 from '@/images/landingpage/gaming.svg'
+import img5 from '@/images/landingpage/flagship.svg'
+import img4 from '@/images/landingpage/robotics.svg'
 
 const Events = () => {
     return (
         <div>
             <div className={style.wrapper}>
                 <div className={`${style.box} ${style.a}`} >
-                    <div id="a"><Image src="../images/landingpage/Techtrix robotics.svg" height={100} width={100} alt="" priority /></div>
+                    <div className={style.element}><Image src={img1} height={437} width={440} alt="" priority /><h3>Automata</h3></div>
                 </div>
                 <div className={`${style.box} ${style.b}`} >
-                    <div id="b">B</div>
+                    <div className={style.element}><Image src={img3} height={470} width={480} alt="" priority /><h3>Automata</h3></div>
                 </div>
                 <div className={`${style.box} ${style.c}`} >
-                    <div id="c">C</div>
+                    <div className={style.element}><Image src={img2} height={437} width={440} alt="" priority /><h3>Automata</h3></div>
                 </div>
                 <div className={`${style.box} ${style.d}`} >
-                    <div id="d">D</div>
+                    <div className={style.element}><Image src={img4} height={437} width={440} alt="" priority /><h3>Automata</h3></div>
                 </div>
                 <div className={`${style.box} ${style.e}`}>
-                    <div id="e">E</div>
+                    <div className={style.element}><Image src={img5} height={437} width={440} alt="" priority /><h3>Automata</h3></div>
                 </div>
             </div>
         </div>
