69f89c3676aadc35ea018055825a647295b335ea
Arghya <arghyadutta080@gmail.com>
2023-03-21 01:55:35 +0530

    wanna add images in Event section

--- components/LandingPage/Events.jsx
+++ components/LandingPage/Events.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import style from '@/styles/Event.module.css'
 
 const Events = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div className={style.wrapper}>
+                <div className={`${style.box} ${style.a}`} >
+                    <div id="a">A</div>
+                </div>
+                <div className={`${style.box} ${style.b}`} >
+                    <div id="b">B</div>
+                </div>
+                <div className={`${style.box} ${style.c}`} >
+                    <div id="c">C</div>
+                </div>
+                <div className={`${style.box} ${style.d}`} >
+                    <div id="d">D</div>
+                </div>
+                <div className={`${style.box} ${style.e}`}>
+                    <div id="e">E</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Events
