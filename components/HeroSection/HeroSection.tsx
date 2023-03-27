eee2ba7e31d4f4997548cf605d342106c82929ee
Shourya Shikhar <danger.ahead@pm.me>
2023-03-27 18:11:43 +0530

    changed register now to view dashboard

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -2,12 +2,21 @@ import { getUser } from "@/utils/getData";
 import { login } from "@/utils/login";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import data from "../../public/data.json";
 import Button from "../Button";
 
 const HeroSection = () => {
   const router = useRouter();
+  const [loggedIn, setLoggedIn] = React.useState(false);
+
+  useEffect(() => {
+    getUser().then((user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+    });
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -31,7 +40,7 @@ const HeroSection = () => {
             login();
           }
         }}
-        text="Register Now"
+        text={loggedIn ? "View Dashboard!" : "Register Now!"}
       />
     </div>
   );
