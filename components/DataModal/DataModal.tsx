ca0252105d4afb409c127f9690217a5cbf777c39
Alimpan De <de.alimpan5@gmail.com>
2023-03-30 04:43:16 +0530

    updated:event modal

--- components/DataModal/DataModal.tsx
+++ components/DataModal/DataModal.tsx
@@ -10,7 +10,8 @@ const DataModal = ({
   eventData: any;
   closeModal: any;
 }) => {
-  console.log(showModal);
+
+  const regIconLink = "https://img.icons8.com/fluency/48/null/money-yours";
 
   return (
     <>
@@ -43,6 +44,18 @@ const DataModal = ({
                 Close
               </button>
             </div>
+            <p
+            className="text-base text-white py-2 text-left px-6 flex flex-row"
+            >
+            <img
+							style={{marginRight: '10px' }}
+							src={`${regIconLink}.png`}
+							alt="money"
+              width={20}
+              height={20}
+						/>
+              <b>Registration Fees :</b> ₹ {eventData.fees}
+            </p>
             <div className="w-full h-96 flex " style={{ overflowY: "scroll" }}>
               <p
                 className="text-base text-white py-2 text-left px-6"
