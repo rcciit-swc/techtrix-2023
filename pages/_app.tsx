98c41150858d5d41851498c8857415b71c9aa31c
Shourya Shikhar <danger.ahead@pm.me>
2023-03-31 22:58:38 +0530

    added ssr page loading animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,39 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "react-toastify/dist/ReactToastify.css";
+import Router from "next/router";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "@/components/LoadingSpinner/LoadingSpinner";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <span className="flex flex-col justify-center items-center h-screen w-screen">
+          <LoadingSpinner />
+        </span>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
 }
