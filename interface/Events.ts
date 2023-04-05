e2555dd49c052b7e6f625b7c07cf5790657aa86f
Shourya Shikhar <danger.ahead@pm.me>
2023-04-06 01:30:38 +0530

    updated readme

--- interface/Events.ts
+++ interface/Events.ts
@@ -6,13 +6,11 @@ export interface Events {
   name: string;
   type: string;
   team_size: number;
-  coordinator?: any; // type unknown;
   rules_regulations: string;
   is_open: boolean;
   poster_image?: string;
   category?: string /* foreign key to event_categories.name */;
   details?: string;
-  convenor?: string /* foreign key to users.id */;
   min_team_size: number;
   event_categories?: Event_categories;
   multiple_registrations_allowed?: boolean;
